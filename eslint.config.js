35cb6c0d85507a69721fb500193ffee7ddc026e0
Pablo Cubillos <pcubillos326@gmail.com>
2024-07-08 19:36:23 +0200

    Adding more configurations and schema with type generator

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,11 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
 import eslintConfigPrettier from "eslint-config-prettier";
 
-
-export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+export default tseslint.config(
+  eslint.configs.recommended,
   eslintConfigPrettier,
-  pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-];
+);
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,25 @@
-import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { ApolloServer } from '@apollo/server'
+import { startStandaloneServer } from '@apollo/server/standalone'
 
-import { readFileSync } from "fs";
-import path from "path";
-import { gql } from "graphql-tag";
+import { readFileSync } from 'fs'
+import path from 'path'
+import { gql } from 'graphql-tag'
+
+const __dirname = path.resolve()
 
 const typeDefs = gql(
-  readFileSync(path.resolve(__dirname, "./schema.graphql"), {
-    encoding: "utf-8",
+  readFileSync(path.resolve(__dirname, './src/schema.graphql'), {
+    encoding: 'utf-8',
   })
-);
+)
 
 async function startApolloServer() {
-  const server = new ApolloServer({ typeDefs });
-  const { url } = await startStandaloneServer(server);
+  const server = new ApolloServer({ typeDefs })
+  const { url } = await startStandaloneServer(server)
   console.log(`
-    🚀  Server is running!
+    🚀  Server is running
     📭  Query at ${url}
-  `);
+  `)
 }
 
 startApolloServer()
